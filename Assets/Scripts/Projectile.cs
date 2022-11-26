a2821ad2b1852d692872986498b05bef9107d5a0
VladimirPirozhenko <vvpirozhok@mail.ru>
2022-11-26 13:38:14 +0300

    Added weapons and projectile

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Projectile : MonoBehaviour, IDamageDealer
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float damage;
+    [SerializeField] private LayerMask collisionToIgore;
+    private Rigidbody2D rb;
+    private Collider2D col;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        Destroy(this.gameObject, 3);
+        transform.SetParent(null);
+    }
+
+    private void Update()
+    {
+        // Debug.Log("ProjectileVelocity:" + rb.velocity.ToString());
+    }
+
+    public void MoveInDirection(Vector2 direction)
+    {
+        rb.velocity = direction.normalized * speed;
+
+    }
+
+    public void IgnoreCollision(Collider2D colliderToIgnore)
+    {
+        Physics2D.IgnoreCollision(colliderToIgnore, GetComponent<Collider2D>(), true);
+    }
+
+    public void DealDamage(IDamageable damageable)
+    {
+        damageable.TakeDamage(damage);
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private Projectile projectilePrefab;
+    [SerializeField] private Transform projectileSpawnLocation;
+    [SerializeField] private int totalAmmo;
+    [SerializeField] private float fireRate;
+   // [SerializeField] private LayerMask collisionToIgore;
+
+    private int currentAmmo;
+    private bool isOnCooldown = false;
+
+    private IEnumerator CooldownRoutine()
+    {
+        isOnCooldown = true;
+        while (isOnCooldown)
+        {
+            yield return new WaitForSeconds(fireRate);
+            isOnCooldown = false;
+        }
+    }
+
+    public void Shoot()
+    {
+        if (isOnCooldown)
+            return;
+
+        Projectile projectile = GameObject.Instantiate(projectilePrefab, this.transform, false);
+        projectile.IgnoreCollision(GetComponent<Collider2D>());
+        projectile.transform.position = projectileSpawnLocation.transform.position;
+        projectile.transform.SetParent(null);
+        projectile.gameObject.SetActive(true);
+        projectile.MoveInDirection(transform.right);
+
+        StartCoroutine(CooldownRoutine());
+    }
+}
